548f2b96733b6bf7c640cf170e108dd2845c6c7b
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-23 16:25:23 -0400

    Conveyor and mirror

--- modifier.gd
+++ modifier.gd
@@ -2,7 +2,7 @@ extends Node
 
 var mirror := false # spike shadows appear on the roof instead of the floor, but spikes still come from the floor
 var ate_their_veggies := false # spikes are extra tall
-var conveyor := false # spike shadows slowly drift
+var conveyor := true # spike shadows slowly drift
 var fatass := false # no jumping
 var good_memory := false # spike shadows slowly fade away
 var waltz := false # spikes only take 3 ticks to appear
--- spike_spawner.gd
+++ spike_spawner.gd
@@ -5,32 +5,59 @@ var SPIKE = preload("res://spike/spike.tscn")
 func _ready() -> void:
 	Pulse.pulse.connect(on_pulse)
 
+func _process(delta: float) -> void:
+	
+	if Modifier.conveyor:
+		
+		for spike in get_children():
+			
+			spike.position.x += 15.0 * delta * spike.get_meta("direction", 1)
+			
+			if abs(spike.position.x) >= 60:
+				spike.set_meta("direction", -spike.get_meta("direction", 1))
+
 func on_pulse(i):
 	
 	if i == -1:
 		
 		for spike in get_children():
+			
 			remove_child(spike)
 			spike.queue_free()
 	
 	if i == 0:
 		
+		if Modifier.mirror:
+			
+			position.y = 60
+			scale.y = 1
+		
 		modulate = Color(1.0, 1.0, 1.0)
 		
 		for spike in get_children():
+			
 			spike.get_node("Collider").disabled = false
 		
 	elif i == 1:
+		
+		if Modifier.mirror:
+			position.y = -60
+			scale.y = -1
 		
 		modulate = Color(0.3, 0.3, 0.3)
 		
 		# delete previous spikes
 		for spike in get_children():
+			
 			remove_child(spike)
 			spike.queue_free()
 		
 		# spawn new spikes
 		for n in range(0, 4):
+			
 			var spike = SPIKE.instantiate()
 			add_child(spike)
 			spike.position.x = RandomNumberGenerator.new().randi_range(-60, 60)
+			
+			if Modifier.conveyor:
+				spike.set_meta("direction", RandomNumberGenerator.new().randi_range(0, 1) * 2 - 1)
