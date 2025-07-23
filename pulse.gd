47a807dcf7af429d7245b51ea7c057a89be3bad0
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-23 16:37:49 -0400

    Title screen

--- pulse.gd
+++ pulse.gd
@@ -5,10 +5,17 @@ signal pulse
 var bpm := 80 # 80 lowest, 180 highest
 
 var _secs_to_next_pulse := 60.0 / bpm
-var _pulse_index := 1
+var _pulse_index := -1
 
 func pause():
+	
 	_pulse_index = -1
+	
+	if bpm < 180:
+		bpm += 5
+		
+		if bpm < 120:
+			bpm += 5
 
 func play():
 	_pulse_index = 0
