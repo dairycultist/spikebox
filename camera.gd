d766d7063e40b22fbbda7b0a9f76e77ab0840eae
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-23 00:53:11 -0400

    Dynamic camera zoom

--- camera.gd
+++ camera.gd
@@ -1,10 +1,14 @@
 extends Camera2D
 
+var zoom_base := 2.0
+
 func _ready() -> void:
 	
 	Pulse.pulse.connect(on_pulse)
 
 func _process(delta: float) -> void:
+	
+	zoom_base = get_viewport_rect().size.x / 400.0
 	
 	# rotation with movement
 	var target_rot := 0.0
@@ -17,14 +21,14 @@ func _process(delta: float) -> void:
 	rotation = lerp(rotation, target_rot, delta)
 	
 	# change size with pulse
-	zoom.x = lerp(zoom.x, 2.0, delta * 10.0)
-	zoom.y = lerp(zoom.x, 2.0, delta * 10.0)
+	zoom.x = lerp(zoom.x, zoom_base, delta * 10.0)
+	zoom.y = lerp(zoom.y, zoom_base, delta * 10.0)
 
 func on_pulse(i):
 	
 	if (i == 0):
-		zoom.x = 2.3
-		zoom.y = 2.3
+		zoom.x = zoom_base * 1.15
+		zoom.y = zoom_base * 0.9
 	else:
-		zoom.x = 2.1
-		zoom.y = 2.1
+		zoom.x = zoom_base * 1.02
+		zoom.y = zoom_base * 0.98
