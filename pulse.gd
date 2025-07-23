d019852d72ead63671b09f3ac1c0b0a20b405dd9
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-23 16:13:40 -0400

    Refactor

--- pulse.gd
+++ pulse.gd
@@ -4,18 +4,27 @@ signal pulse
 
 var bpm := 80 # 80 lowest, 180 highest
 
-var secs_to_next_pulse := 60.0 / bpm
-var pulse_index := 1
+var _secs_to_next_pulse := 60.0 / bpm
+var _pulse_index := 1
+
+func pause():
+	_pulse_index = -1
+
+func play():
+	_pulse_index = 0
 
 func _process(delta: float) -> void:
 	
-	secs_to_next_pulse -= delta
+	_secs_to_next_pulse -= delta
 	
-	if (secs_to_next_pulse <= 0.0):
+	if (_secs_to_next_pulse <= 0.0):
 		
-		pulse.emit(pulse_index)
-		pulse_index = (pulse_index + 1) % (3 if Modifier.waltz else 4)
-		secs_to_next_pulse = 60.0 / bpm
+		pulse.emit(_pulse_index)
+		_secs_to_next_pulse = 60.0 / bpm
 		
-		if Modifier.no_rhythm:
-			secs_to_next_pulse *= RandomNumberGenerator.new().randf_range(0.5, 1.2)
+		if _pulse_index != -1:
+			
+			_pulse_index = (_pulse_index + 1) % (3 if Modifier.waltz else 4)
+		
+			if Modifier.no_rhythm:
+				_secs_to_next_pulse *= RandomNumberGenerator.new().randf_range(0.5, 1.2)
--- spike_spawner.gd
+++ spike_spawner.gd
@@ -7,6 +7,12 @@ func _ready() -> void:
 
 func on_pulse(i):
 	
+	if i == -1:
+		
+		for spike in get_children():
+			remove_child(spike)
+			spike.queue_free()
+	
 	if i == 0:
 		
 		modulate = Color(1.0, 1.0, 1.0)
@@ -16,15 +22,15 @@ func on_pulse(i):
 		
 	elif i == 1:
 		
+		modulate = Color(0.3, 0.3, 0.3)
+		
 		# delete previous spikes
 		for spike in get_children():
 			remove_child(spike)
-			spike.queue_free() 
+			spike.queue_free()
 		
 		# spawn new spikes
 		for n in range(0, 4):
 			var spike = SPIKE.instantiate()
 			add_child(spike)
 			spike.position.x = RandomNumberGenerator.new().randi_range(-60, 60)
-		
-		modulate = Color(0.3, 0.3, 0.3)
