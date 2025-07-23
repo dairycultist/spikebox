db1b631c8ede2c61e0a19cca2af05813bc78a4dc
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-23 17:14:40 -0400

    Fix a playback error with Ping where it would try to play audio when out of scene tree

--- ping/ping.gd
+++ ping/ping.gd
@@ -1,5 +1,7 @@
 extends AudioStreamPlayer2D
 
+# maybe rename to pulse_sound idk
+
 var HIGH = preload("res://ping/high.wav")
 var LOW = preload("res://ping/low.wav")
 
@@ -14,3 +16,6 @@ func on_pulse(i):
 		stream = LOW
 	
 	play()
+
+func _exit_tree() -> void:
+	Pulse.pulse.disconnect(on_pulse)
--- points.gd
+++ points.gd
@@ -15,5 +15,22 @@ func get_str() -> String:
 	return str(_points)
 
 func save_and_reset():
-	print("Saving last score as " + str(_points) + " and highscore as XXX")
+	
+	# save both latest score and highscore
+	var prev_highscore := 0
+	
+	# get previous highscore if it exists
+	if FileAccess.file_exists("user://points.txt"):
+		
+		var load_file = FileAccess.open("user://points.txt", FileAccess.READ)
+		
+		load_file.get_line() # dump last score
+		prev_highscore = int(load_file.get_line())
+	
+	# save
+	var save_file = FileAccess.open("user://points.txt", FileAccess.WRITE)
+	
+	save_file.store_line(str(_points))
+	save_file.store_line(str(max(_points, prev_highscore)))
+	
 	_points = -1
